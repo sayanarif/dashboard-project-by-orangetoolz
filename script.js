97c0c43496197cec6689c9f54815453842e139e2
Md Ariful Islam <arif22113@gmail.com>
2022-08-14 23:09:47 +0600

    All Files added

---
+++ script.js
@@ -0,0 +1,35 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+/* Sidenav*/
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var j;
+
+for (j = 0; j < dropdown.length; j++) {
+  dropdown[j].addEventListener("click", function() {
+    this.classList.toggle("sidenav-active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
